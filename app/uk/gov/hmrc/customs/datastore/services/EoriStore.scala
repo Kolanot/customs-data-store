bc300590fdf002076326dd3dc894e8e1dbc7b25d
Dorcsinecz Gabor <gabor.dorcsinecz@digital.hmrc.gov.uk>
2019-04-26 12:49:00 +0100

    MMCA-1382 Added some mongo stuff

---
+++ app/uk/gov/hmrc/customs/datastore/services/EoriStore.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.customs.datastore.services
+
+import uk.gov.hmrc.customs.datastore.domain.{EORI, EORIHistory, EoriHistoryResponse}
+import javax.inject._
+import play.api.libs.json.Json
+import play.modules.reactivemongo.ReactiveMongoComponent
+import reactivemongo.api.indexes.{Index, IndexType}
+import reactivemongo.bson.BSONObjectID
+import uk.gov.hmrc.mongo.ReactiveRepository
+import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+
+class EoriStore  @Inject()(mongoComponent: ReactiveMongoComponent)
+  extends { val EoriFieldsName = classOf[EORIHistory].getDeclaredFields.apply(0).getName }
+    with ReactiveRepository[EoriHistoryResponse, BSONObjectID](
+    collectionName = "dataStore",
+    mongo = mongoComponent.mongoConnector.db,
+    domainFormat = EORIHistory.eoriHistoryResponseFormat,
+    idFormat = ReactiveMongoFormats.objectIdFormats
+  ) {
+
+
+  override def indexes: Seq[Index] = Seq(
+    Index(Seq(EoriFieldsName -> IndexType.Ascending), name = Some(EoriFieldsName + "Index"), unique = true, sparse = true))
+
+  def eoriAssociate(associatedEori:EORI, eoriHistory:EORIHistory): Future[Any] = {
+    findAndUpdate(
+      Json.obj(EoriFieldsName -> associatedEori),
+      Json.obj("$addToSet" -> eoriHistory),
+      upsert = true
+    )
+  }
+
+  def eoriAdd(eoriHistory: EORIHistory):Future[Any] = {
+    insert(EoriHistoryResponse(Seq(eoriHistory)))
+  }
+
+  def eoriGet(eori: EORI): Future[Option[EoriHistoryResponse]] = {
+    find(EoriFieldsName -> eori).map(_.headOption)
+  }
+
+}
---
+++ test/uk/gov/hmrc/customs/datastore/services/EoriStoreSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.customs.datastore.services
+
+import org.scalatest.{FlatSpec, MustMatchers}
+import org.scalatestplus.play.PlaySpec
+import play.api.test.{DefaultAwaitTimeout, FutureAwaits}
+import play.modules.reactivemongo.ReactiveMongoComponent
+import uk.gov.hmrc.customs.datastore.domain.EORIHistory
+import uk.gov.hmrc.mongo.MongoConnector
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class EoriStoreSpec extends FlatSpec with MustMatchers with MongoSpecSupport with DefaultAwaitTimeout with FutureAwaits {
+
+  val reactiveMongo = new ReactiveMongoComponent {
+    override def mongoConnector: MongoConnector = mongoConnectorForTest
+  }
+  val cache = new EoriStore(reactiveMongo)
+  val eori1 = "EORI12345678"
+
+  it must "put a single EORI into the database" in {
+    val newEori = EORIHistory(eori1, Some("1985-03-20T19:30:51Z"),None)
+    val result = for {
+      _ <- cache.eoriAdd(newEori)
+      storedEori <- cache.eoriGet(eori1)
+    } yield storedEori
+    result.foreach(a => println(s"### newEori: $a"))
+  }
+
+}
